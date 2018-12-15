86ebdcfde835bdd9913943d7af62ddb2aa294d23
libe8013 <2233047175@qq.com>
2018-12-15 17:06:38 +0800

    ssm框架

---
+++ src/test/java/com/zking/erp/base/util/BaseTestCase.java
@@ -0,0 +1,16 @@
+package com.zking.erp.base.util;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext.xml"})
+public class BaseTestCase {
+
+    @Before
+    public void setUp() throws Exception{
+
+    }
+}
