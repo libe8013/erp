d8c70dad86167dcbfdb6bd6d8bd285341e21f6d1
libe8013 <2233047175@qq.com>
2018-12-17 15:41:49 +0800

    git

--- src/main/webapp/basic/js/goodsType.js
+++ src/main/webapp/basic/js/goodsType.js
@@ -2,21 +2,18 @@ layui.use(['table','form','layer','jquery'],function () {
     $ = layui.jquery;
     var table = layui.table;
     initTable(table);
-    queryGoodsType(table);
     $(document).on('click','#goodsTypeQuery',function () {
         queryGoodsType(table);
     })
 });
 
 function initTable(table){
-    var name = $('#name').val();
-    if(name!=null && ''!=name){
-        var render = table.render({
+        table.render({
             elem : '#goodsTypeTab',
             height: 312,
             toolbar : '#toolbarTop',
-            id : 'goodsType',
-            url: '', //数据接口
+            id : 'goodsTypeTable',
+            // url: '', //数据接口
             page: true, //开启分页
             cols: [[ //表头
                 {type:'checkbox', fixed: 'left',width:'4%'},
@@ -25,18 +22,16 @@ function initTable(table){
                 {field:'操作', width:'50%', title: '操作',align:'center',toolbar: '#crud'}
             ]],
         });
-    }
 
 }
 
 function queryGoodsType(table) {
-    table.reload("goodsType", { //此处是上文提到的 初始化标识id
-        where: {
-            //key: { //该写法上文已经提到
-            url: '/erp/goodsType/queryGoodsTypePager'
-            //}
-        }
+    var name = $('#name').val();
+    var url = '/erp/goodsType/queryGoodsTypePager';
+    if(null!=name && ''!=name){
+        url+='?name='+name;
+    }
+    table.reload("goodsTypeTable", { //此处是上文提到的 初始化标识id
+        url: url
     });
-    var render = table.render({});
-    console.log(render);
 }
